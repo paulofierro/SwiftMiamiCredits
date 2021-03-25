d37e276676ff54443b9a347c2b5a1a0a2443a5a0
Paulo Fierro <paulo@paulofierro.com>
2021-03-25 14:52:36 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Credits",
+  platforms: [.iOS(.v14)],
+  products: [
+    .library(
+      name: "Credits", 
+      targets: ["Credits"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "Credits",
+      url: "https://github.com/paulofierro/SwiftMiamiCredits/releases/download/1.0/Credits.xcframework.zip",
+      checksum: "0e28b3dfa2426f6b583a9289558f297f42bfa66f111ecdd0469a94593985e727"
+    )
+  ]
+)
